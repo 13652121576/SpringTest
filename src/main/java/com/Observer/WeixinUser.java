af3f80a0ce17ae2fe874631f2070d26011aa37f3
yudongming <yudongming@peogoo.com>
2020-11-06 13:10:46 +0800

    观察者模式

---
+++ src/main/java/com/Observer/WeixinUser.java
@@ -0,0 +1,18 @@
+package com.Observer;
+
+/**
+ * 观察者
+ */
+public class WeixinUser implements Observer {
+    // 微信用户名
+    private String name;
+    public WeixinUser(String name) {
+        this.name = name;
+    }
+    @Override
+    public void update(String message) {
+        System.out.println(name + "-观察到" + message);
+    }
+
+
+}
